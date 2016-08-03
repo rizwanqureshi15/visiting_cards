cfbd159f76355db7f1577b51ca12abe0a60c96da
rizwanqureshi15 <rizwanqureshi15@gmail.com>
2016-08-03 16:24:59 +0530

    Laravel Basic Authentication
    Laravel Basic Authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('username')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
