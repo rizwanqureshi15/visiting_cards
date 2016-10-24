f2ae2e3560599360759fa80c5fc1d5400aba01e2
rizwanqureshi15 <rizwanqureshi15@gmail.com>
2016-10-24 18:42:23 +0530

    User side Authentication

--- app/Http/Controllers/Front/PaymentsController.php
+++ app/Http/Controllers/Front/PaymentsController.php
@@ -84,14 +84,64 @@ class PaymentsController extends Controller
             // if ($result === FALSE) { /* Handle error */ } 
             
             // dd($result); 
-            
+                 
+        }
 
-        }   
+    }   
 
         public function test()
         {
+           
+// $url = 'https://www.payumoney.com/payment/op/getPaymentResponse?merchantKey= eM8ZaP&merchantTransactionIds=563445'; 
+// $data =array('key'=>'', 'txnid '=>'563445'); 
+// $query = http_build_query($data);
+// $options = array( 
+//   'http' => array( 
+//     'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+//                     "Content-Length: ".strlen($query)."\r\n".
+//                     "User-Agent:MyAgent/1.0\r\n,Authorization: 0SC8FamYqWnwFzVgYKmiCfSsT96xerU8E+WBUh/KDXc=", 
+//     'method' => 'POST', 
+//     'Authorization'=> '0SC8FamYqWnwFzVgYKmiCfSsT96xerU8E+WBUh/KDXc=', 
+//     'content' =>  $query
+//     ), 
+//   ); 
+// $context = stream_context_create($options); 
+// $result = file_get_contents($url, false, $context); 
+// if ($result === FALSE) { /* Handle error */ } 
+// var_dump($result); 
+        // $url = 'https://test.payu.in/_payment'; 
+        //     $data =array('key'=>'JBZaLc', 'txnid'=>'1', 'amount' => '100','productinfo' => 'cards', 'firstname' => 'rizwan', 'email' => 'rizwanqureshi15@gmail.com', 'phone' => '9979557690', 'surl' => url('admin/employees/login'), 'furl' => url('admin/employees/list'), 'service_provider' => 'payu_paisa', 'hash' => Hash('sha512','JBZaLc|1|100|cards|rizwan|rizwanqureshi15@gmail.com|||||||||||GQs7yium')); 
+        //      $query=http_build_query($data) ;
+        //     $options = array( 
+        //       'http' => array( 
+        //         'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+        //             "Content-Length: ".strlen($query)."\r\n".
+        //             "User-Agent:MyAgent/1.0\r\n,Authorization: GQs7yium", 
+        //         'method' => 'POST', 
+        //         'Authorization'=> 'GQs7yium', 
+        //         'content' => $query 
+        //         ), 
+        //       ); 
+        //     $context = stream_context_create($options); 
+        //     $result = file_get_contents($url, false, $context, -1 , 40000); 
+        //     if ($result === FALSE) { /* Handle error */ } 
+            
+        //     dd($result);
+        $data['details'] = [
+            'key'=>'JBZaLc', 
+            'txnid'=>'1', 
+            'amount' => '100',
+            'productinfo' => 'cards', 
+            'firstname' => 'rizwan', 
+            'email' => 'rizwanqureshi15@gmail.com', 
+            'phone' => '9979557690', 
+            'surl' => url('admin/employees/login'), 
+            'furl' => url('admin/employees/list'), 
+            'service_provider' => 'payu_paisa',
+            'hash' => strtolower(hash('sha512','JBZaLc|1|100|cards|rizwan|rizwanqureshi15@gmail.com|||||||||||GQs7yium'))
+            ];
 
-        }
+        return view('payment.payment',$data);
     }
     
 }
