40ebbe6059cd1089caf6e84a6989a301a1ae61d4
rizwanqureshi15 <rizwanqureshi15@gmail.com>
2016-12-10 17:22:44 +0530

    Bug fixing

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -273,15 +273,15 @@ class EmployeeController extends Controller
         $data['order'] = $order;
         $user = $data['user']; 
 
-        Mail::send('emails.confirmation_email', $data , function ($m) use ($user) 
-        {
-            $m->to($user->email, $user->first_name." ".$user->last_name)->subject('Order Confirmation');
-        });
-        //dd(view('emails.admin_confirmation_email', $data)->render());
-        Mail::send('emails.admin_confirmation_email', $data , function ($m) use ($user) 
-        {
-            $m->to(Config::get('settings.admin_email'),'Admin')->subject('Order Confirmation');
-        });
+        // Mail::send('emails.confirmation_email', $data , function ($m) use ($user) 
+        // {
+        //     $m->to($user->email, $user->first_name." ".$user->last_name)->subject('Order Confirmation');
+        // });
+        // //dd(view('emails.admin_confirmation_email', $data)->render());
+        // Mail::send('emails.admin_confirmation_email', $data , function ($m) use ($user) 
+        // {
+        //     $m->to(Config::get('settings.admin_email'),'Admin')->subject('Order Confirmation');
+        // });
 
         Order::where('id', $id)->update(['is_confirmed' => '1', 'status' => Config::get('status.confirmed')]);
         return redirect('new_orders/list');
@@ -431,15 +431,15 @@ class EmployeeController extends Controller
             $user = User::where('id', $data['order']->user_id)->first();
             $data['user'] = $user;
 
-            Mail::send('emails.done_order_email', $data , function ($m) use ($user) 
-            {
-                $m->to($user->email, $user->first_name." ".$user->last_name)->subject('Order is Delivered');
-            });
+            // Mail::send('emails.done_order_email', $data , function ($m) use ($user) 
+            // {
+            //     $m->to($user->email, $user->first_name." ".$user->last_name)->subject('Order is Delivered');
+            // });
             
-            Mail::send('emails.admin_done_order_email', $data , function ($m) use ($user) 
-            {
-                $m->to(Config::get('settings.admin_email'),'Admin')->subject('Order is Delivered');
-            });
+            // Mail::send('emails.admin_done_order_email', $data , function ($m) use ($user) 
+            // {
+            //     $m->to(Config::get('settings.admin_email'),'Admin')->subject('Order is Delivered');
+            // });
         Order::where('id', $id)->update(['status' => Config::get('status.done')]);  
            return redirect()->back();   
         }
--- public/assets/js/order_snap.js
+++ public/assets/js/order_snap.js
@@ -2,39 +2,37 @@ var element = $('#a4');
 var order_id;
 var count=0;
 var a=0;
-alert(card_type);
-$(document).ready(function(){
-	var i = 0;
-	$('#user_overlay').show();
-	$('#admin_navbar').hide();
-	$.each(cards,function(k, card){
-		$.each(card,function(key, value){
-			if(key == "order_id")
-			{
-				order_id = value; 
-			}
-			if(key == "front_snap")
-			{
-                count++;
-				i++;
-				$('#image_'+i).attr('src', site_url+"/order/"+username+"/"+ order_no +"/front/"+value);
-			}
-			if(key == "back_snap" && value != "")
-			{
-                count++;
-				i++;
-				
-			}
-		});
-        if(cards.length != count)
-        {
-            a=1;
-        }
-		if(i == 10 && cards.length != count)
-		{
-			i = 0;
 
-			html2canvas(element, {
+$(document).ready(function(){
+	if(card_type == 'horizontal')
+    {
+        var i = 0;
+    $('#user_overlay').show();
+    $('#admin_navbar').hide();
+    $.each(cards,function(k, card){
+        $.each(card,function(key, value){
+            if(key == "order_id")
+            {
+                order_id = value; 
+            }
+            if(key == "front_snap")
+            {
+                count++;
+                i++;
+                $('#image_'+i).attr('src', site_url+"/order/"+username+"/"+ order_no +"/front/"+value);
+            }
+            if(key == "back_snap" && value != "")
+            {
+                count++;
+                i++;
+                $('#image_'+i).attr('src', site_url+"/order/"+username+"/"+ order_no +"/back/"+value);
+            }
+        });
+        if(i == 10 && cards.length != count)
+        {
+            i = 0;
+
+            html2canvas(element, {
                 onrendered: function (canvas) {
                 getCanvas = canvas;
                 var imgageData = getCanvas.toDataURL("image/png");
@@ -46,21 +44,40 @@ $(document).ready(function(){
                 data: {"_token": token ,"image": imgageData, "order_id":order_id},
                 success : function(image)
                 {
+                    if(cards.length == count)
+                    {
+                        $.ajax({
+                            type: "POST",
+                            url: site_url+'/change_status',
+                            dataType: 'json',
+                            async: true,
+                            data: {"_token": token , "order_id":order_id, "status": "done"},
+                            success : function(image)
+                            {
+                                
+                               window.location.href = site_url+"/admin/orders/final/"+order_id+"/list";
+                                $('#user_overlay').hide();
+                                $('#admin_navbar').show();
 
+                            }}).fail(function(data){
+                        
+                                var errors = data.responseJSON;
+                            });
+                    }
                 }}).fail(function(data){
                     
                     var errors = data.responseJSON;
                 });
             }});
-		}
-	});
-	var j;
-	for (j = i+1; j<=10 ; j++)
-	{
-		$('#image_'+j).attr('src', " ");
-	}
-	html2canvas(element, {
-      	onrendered: function (canvas) {
+        }
+    });
+    var j;
+    for (j = i+1; j<=10 ; j++)
+    {
+        $('#image_'+j).attr('src', " ");
+    }
+    html2canvas(element, {
+        onrendered: function (canvas) {
         getCanvas = canvas;
         var imgageData = getCanvas.toDataURL("image/png");
         $.ajax({
@@ -68,35 +85,144 @@ $(document).ready(function(){
             url: site_url+'/card_list_snap',
             dataType: 'json',
             async: false,
-            data: {"_token": token ,"image": imgageData, "order_id":order_id, "status": "done"},
+            data: {"_token": token ,"image": imgageData, "order_id":order_id},
             success : function(image)
             {
-    
+                if(cards.length == count)
+                {
+                    $.ajax({
+                    type: "POST",
+                    url: site_url+'/change_status',
+                    dataType: 'json',
+                    async: true,
+                    data: {"_token": token , "order_id":order_id, "status": "done"},
+                    success : function(image)
+                    {
+                        
+                       window.location.href = site_url+"/admin/orders/final/"+order_id+"/list";
+                        $('#user_overlay').hide();
+                        $('#admin_navbar').show();
 
+                    }}).fail(function(data){
+                
+                        var errors = data.responseJSON;
+                    });
+                }
             }}).fail(function(data){
         
                 var errors = data.responseJSON;
             });
     }});
-if(a == 1)
-{
-    $.ajax({
+    }
+    else
+    {
+        var i = 0;
+    $('#user_overlay').show();
+    $('#admin_navbar').hide();
+    $.each(cards,function(k, card){
+        $.each(card,function(key, value){
+            if(key == "order_id")
+            {
+                order_id = value; 
+            }
+            if(key == "front_snap")
+            {
+                count++;
+                i++;
+                $('#image_'+i).attr('src', site_url+"/order/"+username+"/"+ order_no +"/front/"+value);
+            }
+            if(key == "back_snap" && value != "")
+            {
+                count++;
+                i++;
+                $('#image_'+i).attr('src', site_url+"/order/"+username+"/"+ order_no +"/back/"+value);
+            }
+        });
+        if(i == 6 && cards.length != count)
+        {
+            i = 0;
+
+            html2canvas(element, {
+                onrendered: function (canvas) {
+                getCanvas = canvas;
+                var imgageData = getCanvas.toDataURL("image/png");
+                $.ajax({
+                type: "POST",
+                url: site_url+'/card_list_snap',
+                dataType: 'json',
+                async: false,
+                data: {"_token": token ,"image": imgageData, "order_id":order_id},
+                success : function(image)
+                {
+                    if(cards.length == count)
+                    {
+                        $.ajax({
+                            type: "POST",
+                            url: site_url+'/change_status',
+                            dataType: 'json',
+                            async: true,
+                            data: {"_token": token , "order_id":order_id, "status": "done"},
+                            success : function(image)
+                            {
+                                
+                               window.location.href = site_url+"/admin/orders/final/"+order_id+"/list";
+                                $('#user_overlay').hide();
+                                $('#admin_navbar').show();
+
+                            }}).fail(function(data){
+                        
+                                var errors = data.responseJSON;
+                            });
+                    }
+                }}).fail(function(data){
+                    
+                    var errors = data.responseJSON;
+                });
+            }});
+        }
+    });
+    var j;
+    for (j = i+1; j<=6 ; j++)
+    {
+        $('#image_'+j).attr('src', " ");
+    }
+    html2canvas(element, {
+        onrendered: function (canvas) {
+        getCanvas = canvas;
+        var imgageData = getCanvas.toDataURL("image/png");
+        $.ajax({
             type: "POST",
-            url: site_url+'/change_status',
+            url: site_url+'/card_list_snap',
             dataType: 'json',
-            async: true,
-            data: {"_token": token , "order_id":order_id, "status": "done"},
+            async: false,
+            data: {"_token": token ,"image": imgageData, "order_id":order_id},
             success : function(image)
             {
-                
-               window.location.href = site_url+"/admin/orders/final/"+order_id+"/list";
-                $('#user_overlay').hide();
-                $('#admin_navbar').show();
+                if(cards.length == count)
+                {
+                    $.ajax({
+                    type: "POST",
+                    url: site_url+'/change_status',
+                    dataType: 'json',
+                    async: true,
+                    data: {"_token": token , "order_id":order_id, "status": "done"},
+                    success : function(image)
+                    {
+                        
+                       window.location.href = site_url+"/admin/orders/final/"+order_id+"/list";
+                        $('#user_overlay').hide();
+                        $('#admin_navbar').show();
 
+                    }}).fail(function(data){
+                
+                        var errors = data.responseJSON;
+                    });
+                }
             }}).fail(function(data){
         
                 var errors = data.responseJSON;
             });
-}
+    }});
+    }
     
 });
